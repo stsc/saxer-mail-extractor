583be5fecd0f6c9c013d9c12e4d16fd14e909eb6
Robin Cerny <robin@cerny.li>
2023-07-02 01:46:07 +0200

    improved entrypoint.sh error handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+# turn on bash's job control
+set -m
 
 # config example handling
-[ ! -r /config/crontab ] && cp /config.example/crontab /config/crontab || exit 1
-[ ! -r /config/mail_extractor.cfg ] && cp /config.example/mail_extractor.cfg  /config/mail_extractor.cfg || exit 1
+if [ ! -r /config/crontab ] ; then
+	cp /config.example/crontab /config/crontab
+else
+	echo "config/crontab not readable! exiting..."
+	exit 1
+fi
+if [ ! -r /config/mail_extractor.cfg ] ; then
+	cp /config.example/mail_extractor.cfg /config/mail_extractor.cfg
+else
+	echo "config/mail_extractor.cfg not readable! exiting..."
+	exit 1
+fi
 
 # Copy crontab file to the cron.d directory
 cp /config/crontab /etc/cron.d/crontab
@@ -10,6 +23,9 @@ cp /config/crontab /etc/cron.d/crontab
 chmod 0644 /etc/cron.d/crontab
 # Apply cron job
 crontab /etc/cron.d/crontab
-# execute cron in foreground mode
-cron -f || exit 2
-
+# execute cron in foreground mode and put it in background
+cron -f &
+# wait for all processes to exit
+wait -n
+# Exit with status of process that exited first
+exit $?
