3cfbd769f9ace0168dc59042e54a5ac9d38db190
Robin Cerny <robin@cerny.li>
2023-07-02 01:23:43 +0200

    fixed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # config example handling
 [ ! -r /config/crontab ] && cp /config.example/crontab /config/crontab || exit 1
-[ ! -r /config/mail_extractor.cfg ] && /config.example/mail_extractor.cfg  /config/mail_extractor.cfg || exit 1
+[ ! -r /config/mail_extractor.cfg ] && cp /config.example/mail_extractor.cfg  /config/mail_extractor.cfg || exit 1
 
 # Copy crontab file to the cron.d directory
 cp /config/crontab /etc/cron.d/crontab
