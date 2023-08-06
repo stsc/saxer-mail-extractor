e44e7ceac7d1f73c1cb2e61c47bcee005d5dad4e
Steven Schubiger <stsc@refcnt.org>
2023-08-06 21:53:30 +0200

    Finalize creating and diffing CSV entries

--- app/mail_extractor.pl
+++ app/mail_extractor.pl
@@ -30,10 +30,22 @@ use Getopt::Long qw(:config no_auto_abbrev no_ignore_case);
 use JSON;
 use Net::IDN::Encode qw(email_to_unicode);
 use POSIX qw(strftime);
+use Time::Local qw(timelocal_posix);
 
 my $VERSION = '0.00';
 
 my $CSV_file = 'addresses.csv';
+
+my $read_file = sub
+{
+    my ($file) = @_;
+
+    open(my $fh, '<', $file) or die "$0: file `$file' cannot be opened: $!\n";
+    my $string = do { local $/; <$fh> };
+    close($fh);
+
+    return $string;
+};
 
 my $remove_quotes = sub
 {
@@ -62,7 +74,7 @@ my %config = parse_config($Config_file);
 
 validate_config(\%config);
 
-my $tmpdir = tempdir('mail-extractor.XXXXXXXXXX', TMPDIR => true);
+my $tmpdir = tempdir('mail-extractor.XXXXXXXXXX', TMPDIR => true, CLEANUP => true);
 
 my $csv_file_char = sub
 {
@@ -71,7 +83,22 @@ my $csv_file_char = sub
     return File::Spec->catfile($tmpdir, $csv_file);
 };
 
-my $Incremental = -e File::Spec->catfile($config{output_path}, $CSV_file);
+my $get_last_timestamp = sub
+{
+    my ($time_stamp, $fmt) = @_;
+
+    my @sort;
+    foreach my $csv_file (glob(File::Spec->catfile($config{output_path}, 'addresses_full-*.csv'))) {
+        (undef, undef, my $file) = File::Spec->splitpath($csv_file);
+        $file =~ /^addresses_full-(.+?)\.(.+?)\.(.+?)-(.+?)\.(.+?)\.csv$/ or next;
+        push @sort, [ 0, $5, $4, $1, $2 - 1, $3 - 1900 ];
+    }
+    my @sorted = map strftime($fmt, localtime $_), sort { $b <=> $a } map timelocal_posix(@$_), @sort;
+
+    $$time_stamp = $sorted[0] // '';
+
+    return defined $$time_stamp && length $$time_stamp ? true : false;
+};
 
 open(my $log_fh, '>', $config{log_file}) or die "$0: logging file `$config{log_file}' cannot be opened: $!\n";
 
@@ -82,12 +109,22 @@ select($old_fh);
 
 log_print("Start of %s.", $0);
 
-my $time_stamp = strftime('%d.%m.%Y-%H.%M', localtime);
-my $csv_file_diff = File::Spec->catfile($config{output_path}, "addresses_diff-$time_stamp.csv");
+my $last_time_stamp = undef;
+my $Incremental = $get_last_timestamp->(\$last_time_stamp, '%d.%m.%Y');
+
+my $current_time_stamp = strftime('%d.%m.%Y-%H.%M', localtime);
+
+my $csv_file_full = File::Spec->catfile($config{output_path}, "addresses_full-$current_time_stamp.csv");
+my $csv_file_diff = File::Spec->catfile($config{output_path}, "addresses_diff-$last_time_stamp-$current_time_stamp.csv");
 
 if ($Incremental) {
+    my $time_stamp;
+    $get_last_timestamp->(\$time_stamp, '%d.%m.%Y-%H.%M');
+
+    my $csv_file_last = File::Spec->catfile($config{output_path}, "addresses_full-$time_stamp.csv");
+
     my @entries;
-    read_csv(File::Spec->catfile($config{output_path}, $CSV_file), \@entries);
+    read_csv($csv_file_last, \@entries);
     my %seen = map { $_->[1] => true } @entries;
 
     my @addresses;
@@ -99,6 +136,9 @@ if ($Incremental) {
     read_csv($csv_file_diff, \@addresses);
 
     filter_sort(\@addresses, $config{filter});
+
+    write_csv($csv_file_full, \@addresses);
+    unlink $csv_file_last if $csv_file_last ne $csv_file_full;
 
     @addresses = grep !$seen{$_->[1]}, @addresses;
 
@@ -121,6 +161,14 @@ else {
 
         write_csv($csv_file, \@addresses);
     }
+
+    opendir(my $dh, $tmpdir) or die "$0: temporary directory `$tmpdir' cannot be opened: $!\n";
+    my @csv_files = map File::Spec->catfile($tmpdir, $_), sort grep !/^\.\.?$/, readdir($dh);
+    closedir($dh);
+
+    open(my $fh, '>', $csv_file_full) or die "$0: CSV file `$csv_file_full' cannot be opened: $!\n";
+    print {$fh} $read_file->($_) foreach @csv_files;
+    close($fh);
 }
 
 log_print("End of %s.", $0);
